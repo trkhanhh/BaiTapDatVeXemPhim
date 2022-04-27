50d19634ce88b19e96bf8ced3690690b73c55210
Trong Khanh Mai <maitrongkhanh7@gmail.com>
2022-04-28 02:01:48 +0700

    booking-tickets
    booking-tickets

---
+++ bookingmovietickets/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import BaiTapBookingTicket from "./BaiTapRedux/BaiTapBookingTicket/BaiTapBookingTicket";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BaiTapBookingTicket />
+      </div>
+    );
+  }
+}
---
+++ bookingmovietickets/src/redux/reducer/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+import BaiTapDatVeReducer from "./BaiTapDatVeReducer";
+const rootReducer = combineReducers({
+  BaiTapDatVeReducer,
+});
+
+export default rootReducer;
